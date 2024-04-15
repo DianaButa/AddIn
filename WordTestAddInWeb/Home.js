334ace782d900a9a939c69683b18df89bf229d5e
Diana Buta <diana.c.farcas@gmail.com>
2024-04-15 11:11:34 +0300

    something

--- WordTestAddInWeb/Home.js
+++ WordTestAddInWeb/Home.js
@@ -1,187 +1,220 @@
-﻿//'use strict';
-
-//(function () {
-
-//    Office.onReady(function () {
-//        // Office is ready.
-//        $(document).ready(function () {
-//            // The document is ready.
-//            // Use this to check whether the API is supported in the Word client.
-//            if (Office.context.requirements.isSetSupported('WordApi', '1.1')) {
-//                // Do something that is only available via the new APIs.
-//                $('#getUser').on("click", getUserInfo);
-//                $('#getClient').on("click", getClientInfo);
-//                $('#getCompanyClient').on("click", getCompanyClientInfo);
-//                $('#getDocuments').on("click", getDocumentsInfo);
-//                $('#getFiles').on("click", getFilesInfo);
-//                // Add event listeners for other actions as needed
-//            } else {
-//                // Lets you know that this code will not work with your version of Word.
-//                $('#supportedVersion').html('This code requires Word 2016 or later.');
-//            }
-//        });
-//    });
-
-//    async function getUserInfo() {
-//        try {
-//            const response = await fetch('https://localhost:7129/api/User');
-//            if (!response.ok) {
-//                throw new Error('Network response was not ok');
-//            }
-//            const userData = await response.json();
-//            console.log('User Information:', userData);
-//            // Handle the user data as needed
-//        } catch (error) {
-//            console.error('There was a problem with the fetch operation:', error);
-//        }
-//    }
-
-//    async function getClientInfo() {
-//        try {
-//            const response = await fetch('https://localhost:7129/api/Client');
-//            if (!response.ok) {
-//                throw new Error('Network response was not ok');
-//            }
-//            const clientData = await response.json();
-//            console.log('Client Information:', clientData);
-//            // Handle the client data as needed
-//        } catch (error) {
-//            console.error('There was a problem with the fetch operation:', error);
-//        }
-//    }
-
-//    async function getCompanyClientInfo() {
-//        try {
-//            const response = await fetch('https://localhost:7129/api/CompanyClient');
-//            if (!response.ok) {
-//                throw new Error('Network response was not ok');
-//            }
-//            const companyClientData = await response.json();
-//            console.log('Company Client Information:', companyClientData);
-//            // Handle the company client data as needed
-//        } catch (error) {
-//            console.error('There was a problem with the fetch operation:', error);
-//        }
-//    }
-
-//    async function getDocumentsInfo() {
-//        try {
-//            const response = await fetch('https://localhost:7129/api/Documents');
-//            if (!response.ok) {
-//                throw new Error('Network response was not ok');
-//            }
-//            const documentsData = await response.json();
-//            console.log('Documents Information:', documentsData);
-//            // Handle the documents data as needed
-//        } catch (error) {
-//            console.error('There was a problem with the fetch operation:', error);
-//        }
-//    }
-
-//    async function getFilesInfo() {
-//        try {
-//            const response = await fetch('https://localhost:7129/api/Files');
-//            if (!response.ok) {
-//                throw new Error('Network response was not ok');
-//            }
-//            const filesData = await response.json();
-//            console.log('Files Information:', filesData);
-//            // Handle the files data as needed
-//        } catch (error) {
-//            console.error('There was a problem with the fetch operation:', error);
-//        }
-//    }
-
-//})();
+﻿
 'use strict';
 
-(function () {
-
-    Office.onReady(function () {
-        // Office is ready.
-        $(document).ready(function () {
-            // The document is ready.
-            // Use this to check whether the API is supported in the Word client.
-            if (Office.context.requirements.isSetSupported('WordApi', '1.1')) {
-                // Do something that is only available via the new APIs.
-                $('#emerson').on("click", insertEmersonQuoteAtSelection);
-                $('#checkhov').on("click", insertChekhovQuoteAtTheBeginning);
-                $('#proverb').on("click", insertChineseProverbAtTheEnd);
-                $('#supportedVersion').html('This code is using Word 2016 or later.');
-            } else {
-                // Lets you know that this code will not work with your version of Word.
-                $('#supportedVersion').html('This code requires Word 2016 or later.');
-            }
-        });
+Office.onReady(function () {
+    // Office is ready.
+    $(document).ready(function () {
+        console.log("getClientInfo");
+        // The document is ready.
+        // Use this to check whether the API is supported in the Word client.
+        if (Office.context.requirements.isSetSupported('WordApi', '1.1')) {
+            // Do something that is only available via the new APIs.
+            $('#client').on("click", getClientInfo);
+            $('#documents').on("click", getDocumentsInfo);
+            $('#files').on("click", getFilesInfo);
+            $('#findandreplace').on("click", findAndReplace);
+            $('#supportedVersion').html('This code is using Word 2016 or later.');
+        } else {
+            // Lets you know that this code will not work with your version of Word.
+            $('#supportedVersion').html('This code requires Word 2016 or later.');
+        }
     });
+});
 
-    async function insertEmersonQuoteAtSelection() {
-        await Word.run(async (context) => {
-
-            // Create a proxy object for the document.
-            const thisDocument = context.document;
-
-            // Queue a command to get the current selection.
-            // Create a proxy range object for the selection.
-            const range = thisDocument.getSelection();
-
-            // Queue a command to replace the selected text.
-            range.insertText('"Hitch your wagon to a star." - Ralph Waldo Emerson\n', Word.InsertLocation.replace);
-
-            // Synchronize the document state by executing the queued commands,
-            // and return a promise to indicate task completion.
-            await context.sync();
-            console.log('Added a quote from Ralph Waldo Emerson.');
-        })
-            .catch(function (error) {
-                console.log('Error: ' + JSON.stringify(error));
-                if (error instanceof OfficeExtension.Error) {
-                    console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-                }
+$(document).ready(function () {
+    // Fetch data from the server-side endpoint
+    $.ajax({
+        url: 'https://localhost:7129/api/Client', 
+        method: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var $select = $('#mySelect');
+            $select.empty();
+            // Iterate over the data and append a new option to the select
+            $.each(data, function (key, value) {
+                $select.append($('<option></option>').val(value.firstName).html(value.lastName));
             });
+        },
+        error: function (xhr, status, error) {
+            console.error('Error fetching data:', error);
+        }
+    });
+});
+
+
+
+
+    async function getClientInfo() {
+        try {
+            const response = await fetch('https://localhost:7129/api/Client');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const clientData = await response.json();
+            console.log('Client Information:', clientData);
+
+            //  insert the client information into a table in the document
+            Word.run(async (context) => {
+                const tableData = [];
+
+                clientData.forEach(client => {
+                    tableData.push([client.id, client.firstName, client.lastName, client.adress, client.cnp]);
+                });
+
+                const table = context.document.body.insertTable(clientData.length, 5, "Start", tableData);
+                // table.styleBuiltIn = Word.BuiltInStyleName.gridTable5Dark_Accent2;
+                // Synchronize the document state by executing the queued commands
+                await context.sync();
+            });
+
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
     }
 
-    async function insertChekhovQuoteAtTheBeginning() {
-        await Word.run(async (context) => {
 
-            // Create a proxy object for the document body.
-            const body = context.document.body;
 
-            // Queue a command to insert text at the start of the document body.
-            body.insertText('"Knowledge is of no value unless you put it into practice." - Anton Chekhov\n', Word.InsertLocation.start);
 
-            // Synchronize the document state by executing the queued commands,
-            // and return a promise to indicate task completion.
-            await context.sync();
-            console.log('Added a quote from Anton Chekhov.');
-        })
-            .catch(function (error) {
-                console.log('Error: ' + JSON.stringify(error));
-                if (error instanceof OfficeExtension.Error) {
-                    console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-                }
-            });
+
+    async function getDocumentsInfo() {
+        try {
+            const response = await fetch('https://localhost:7129/api/Documents');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const documentsData = await response.json();
+            console.log('Documents Information:', documentsData);
+
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
+    }
+    async function getFilesInfo() {
+        try {
+            const response = await fetch('https://localhost:7129/api/Files');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const filesData = await response.json();
+            console.log('Files Information:', filesData);
+
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
     }
 
-    async function insertChineseProverbAtTheEnd() {
-        await Word.run(async (context) => {
 
-            // Create a proxy object for the document body.
-            const body = context.document.body;
 
-            // Queue a command to insert text at the end of the document body.
-            body.insertText('"To know the road ahead, ask those coming back." - Chinese proverb\n', Word.InsertLocation.end);
+    async function findAndReplace() {
+        try {
+            
+            const clientData = await getClientDataFromDatabase();
 
-            // Synchronize the document state by executing the queued commands,
-            // and return a promise to indicate task completion.
-            await context.sync();
-            console.log('Added a quote from a Chinese proverb.');
-        })
-            .catch(function (error) {
-                console.log('Error: ' + JSON.stringify(error));
-                if (error instanceof OfficeExtension.Error) {
-                    console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-                }
-            });
+           
+            await replaceMe('<<FirstName>>', clientData.firstName);
+            await replaceMe('<<LastName>>', clientData.lastName);
+            await replaceMe('<<adress>>', clientData.adress);
+            await replaceMe('<<CNP>>', clientData.CNP);
+
+
+        } catch (error) {
+            console.error('Error during findAndReplace:', error);
+        }
     }
-})();
+
+    async function getClientDataFromDatabase() {
+        try {
+            const response = await fetch('https://localhost:7129/api/Client/1');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const clientData = await response.json();
+            console.log('Client Information:', clientData);
+            return clientData;
+        } catch (error) {
+            console.error('Error during getClientDataFromDatabase:', error);
+           
+            return { firstName: "Name", lastName: "Lastname", adress: "adress", CNP: "CNP" };
+        }
+    }
+
+    async function replaceMe(whatToFind, whatToReplaceWith) {
+        try {
+            await Word.run(async (context) => {
+                const searchResults = context.document.body.search(whatToFind, { matchCase: false });
+                context.load(searchResults, 'text, font');
+                await context.sync();
+
+                for (let i = 0; i < searchResults.items.length; i++) {
+                    searchResults.items[i].insertText(whatToReplaceWith, 'Replace');
+                }
+
+                await context.sync();
+            });
+        } catch (error) {
+            console.log('Error during replaceMe:', error);
+            if (error instanceof OfficeExtension.Error) {
+                console.log('Debug info:', error.debugInfo);
+            }
+        }
+    }
+
+
+
+
+//    //async function findAndReplace() {
+//    //    try {
+//    //        const firstNameReplacement = await getFirstNameReplacementFromDatabase();
+//    //        await replaceMe('<<FirstName>>', firstNameReplacement);
+//    //    } catch (error) {
+//    //        console.error('Error during findAndReplace:', error);
+//    //    }
+//    //}
+
+//    //async function getFirstNameReplacementFromDatabase() {
+//    //    try {
+//    //        const response = await fetch('https://localhost:7129/api/Client/1');
+//    //        if (!response.ok) {
+//    //            throw new Error('Network response was not ok');
+//    //        }
+//    //        const clientData = await response.json();
+//    //        console.log('Client Information:', clientData);
+//    //        return clientData.firstName;
+//    //    } catch (error) {
+//    //        console.error('Error during getFirstNameReplacementFromDatabase:', error);
+//    //        return "Name";
+//    //    }
+//    //}
+
+//    //async function replaceMe(whatToFind, whatToReplaceWith) {
+//    //    try {
+//    //        await Word.run(async (context) => {
+//    //            const searchResults = context.document.body.search(whatToFind, { matchCase: false });
+//    //            context.load(searchResults, 'text, font');
+//    //            await context.sync();
+
+//    //            for (let i = 0; i < searchResults.items.length; i++) {
+//    //                searchResults.items[i].insertText(whatToReplaceWith, 'Replace');
+//    //            }
+
+//    //            await context.sync();
+//    //        });
+//    //    } catch (error) {
+//    //        console.log('Error during replaceMe:', error);
+//    //        if (error instanceof OfficeExtension.Error) {
+//    //            console.log('Debug info:', error.debugInfo);
+//    //        }
+//    //    }
+//    //}
+
+
+
+
+//    //Functie replaceme(whatToFind, whatToReplaceWith) { }
+
+//    //replaceMe(<< NANME >>, yourobject.name)
+//    //replaceMe(<< PRENUME >>, yourobject.surname)
+
+
+
+//})();
